d801ca5cc9ae1be783f3f02960b275c20c17bbfe
strojn <anatolii.strojn@gmail.com>
2023-09-26 16:03:42 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    int year;
+    String color;
+    public Car(int year, String color) {
+        this.year = year;
+        this.color = color;
+    }
+    public Car(int year) {
+        this.year = year;
+    }
+    public Car() {
+    }
+}
